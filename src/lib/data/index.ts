7e9a74750aeaab2412ff1601ccdfaa3e2784ea21
Ngoc Tinh Tran <nil-vn@users.noreply.github.com>
2026-05-17 12:29:43 +0900

    Add bundled default N2 vocabulary dataset

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,4 +1,4 @@
-export { starterFlashcards } from './flashcards';
+export { defaultN2Vocabulary, starterFlashcards } from './flashcards';
 export { jlptLevels, levelDescriptions } from './levels';
 export { validateDataset } from './dataset-validator';
 export type { DatasetValidationError, DatasetValidationResult } from './dataset-validator';
